5e62e67681630b078ce6472a388fb16d10dffd01
moha-alsenani <msm_alsenani@icloud.com>
2025-10-01 18:38:12 +0100

    first commit, done basics.

---
+++ basics/lists.py
@@ -0,0 +1,11 @@
+
+animals = ["cat", "Dog", "Giraffe", "My Best Friend", "3s3s"]
+
+print(animals[0])
+print(animals[-1])
+
+animals.append("Lioness")
+print(animals)
+
+animals.remove("Giraffe")
+print(animals)
